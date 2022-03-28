dd7f99286c42d1261590353569db8fb1dddd042a
David Albert <david.albert@insa-lyon.fr>
2022-03-28 17:24:28 +0200

    Udpate state

--- game/state_qLearning.py
+++ game/state_qLearning.py
@@ -1,72 +1,68 @@
+import imp
 import pygame
 import random
-import math
 from pygame import mixer
 import numpy as np
-
+from copy import deepcopy
 
 class State():
 	#collision balle : si distance euclienne <= 50
 	#collision vaisseau-invaders : if invader_Y[i] >= 450: && delta_x < 80:
 	#changeY : 50
 	#changeX_invaders ~= 10
-	def __init__(self, invaders_init_position, player_init_position, nbInvaders):
-		self.invaders_pos = invadars_init_position
+	def __init__(self, player_init_position, nbInvaders):
 		self.nbInvaders = nbInvaders
-		self.last_invaders_pos = [[]]
-		self.player_pos = player_init_position
-		self.invaders_state = [[]]
+		self.player_x = player_init_position[0]
+		self.player_y = player_init_position[1]
+		self.current_state = [(0,0) for _ in range(nbInvaders)]
+		self.last_state = [(0,0) for _ in range(nbInvaders)]
 		self.screen_width = 800
 		self.screen_height = 600
 
-	def update_state(self,invaders_delta_xy,player_delta_xy):
+	def update_state(self, invaders_x, invaders_y, player_x):
 
-		for i in range(nbInvaders):
-			next_invaders_pos = [[]]
-			next_player_pos = [[]]
-			
-			self.last_invaders_pos = self.invaders_pos;
-			self.player_pos[0] = player_pos[0]+player_delta_xy[0]
-			self.player_pos[1] = player_pos[1]+player_delta_xy[1]
+		for i in range(self.nbInvaders):
+			self.last_state = deepcopy(self.current_state)
 
-			invaders_pos[i][0] = invaders_pos[i][0]+invaders_delta_xy[i][0]:
-			invaders_pos[i][1] = invaders_pos[i][1]+invaders_delta_xy[i][1]:
+			zone_x = self.getZone("x", player_x, (invaders_x, invaders_y))
+			zone_y = self.getZone("y", player_x, (invaders_x, invaders_y))
 
-			if (getZone(i,"x")=="veryCloseZone" && (self.player_pos[0]-invaders_pos[i][0]) <0):
-				invaders_state[i][0]=1
-			if (getZone(i,"x")=="veryCloseZone" && (self.player_pos[0]-invaders_pos[i][0]) >0):
-				invaders_state[i][0]=-1
+			if (zone_x=="veryCloseZone" and (player_x-invaders_x[i]) <0):
+				self.current_state[i][0]=1
+			if (zone_x=="veryCloseZone" and (player_x-invaders_x[i]) >0):
+				self.current_state[i][0]=-1
 
-			if (getZone(i,"x")=="midCloseZone" && (self.player_pos[0]-invaders_pos[i][0]) <0):
-				invaders_state[i][0]=2
-			if (getZone(i,"x")=="midCloseZone" && (self.player_pos[0]-invaders_pos[i][0]) >0):
-				invaders_state[i][0]=-2
+			if (zone_x=="midCloseZone" and (player_x-invaders_x[i]) <0):
+				self.current_state[i][0]=2
+			if (zone_x=="midCloseZone" and (player_x-invaders_x[i]) >0):
+				self.current_state[i][0]=-2
 
-			if (getZone(i,"x")=="farCloseZone" && (self.player_pos[0]-invaders_pos[i][0]) <0):
-				invaders_state[i][0]=3
-			if (getZone(i,"x")=="farCloseZone" && (self.player_pos[0]-invaders_pos[i][0]) >0):
-				invaders_state[i][0]=-3
+			if (zone_x=="farCloseZone" and (player_x-invaders_x[i]) <0):
+				self.current_state[i][0]=3
+			if (zone_x=="farCloseZone" and (player_x-invaders_x[i]) >0):
+				self.current_state[i][0]=-3
 
-			if (getZone(i,"y")=="veryCloseZone"):
-				invaders_state[i][1]=1
-			if (getZone(i,"y")=="midCloseZone"):
-				invaders_state[i][1]=2
-			if (getZone(i,"y")=="farCloseZone"):
-				invaders_state[i][1]=3
+			if (zone_y=="veryCloseZone"):
+				self.current_state[i][1]=1
+			if (zone_y=="midCloseZone"):
+				self.current_state[i][1]=2
+			if (zone_y=="farCloseZone"):
+				self.current_state[i][1]=3
 
-		return invaders_state
+		return (self.last_state, self.current_state)
 
-	def getZone(i,s):
+
+	def getZone(self, s, player_x, invaders_i):
 		if (s=="x"):
-			if (math.abs(self.player_pos[0]-invaders_pos[i][0])<120):
+			if (abs(player_x-invaders_i[0])<120):
 				return "veryCloseZone"
-			if (math.abs(self.player_pos[0]-invaders_pos[i][0])<200 && math.abs(self.player_pos[0]-invaders_pos[i][0])<120):
+			if (abs(player_x-invaders_i[0])<200 and abs(self.player_x-invaders_i[0])<120):
 				return "midCloseZone"
 			return "farCloseZone"
 
 		if (s=="y"):
-			if (math.abs(self.player_pos[0]-invaders_pos[i][0])<200):
+			if (abs(self.player_y-invaders_i[1])<200):
 				return "veryCloseZone"
-			if (math.abs(self.player_pos[0]-invaders_pos[i][0])<300 && math.abs(self.player_pos[0]-invaders_pos[i][0])<200):
+			if (abs(self.player_y-invaders_i[1])<300 and abs(self.player_y-invaders_i[1])<200):
 				return "midCloseZone"
 			return "farCloseZone"
